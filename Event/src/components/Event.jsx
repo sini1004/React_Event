4573888c390e8eb7b9ee444c4647df525e7b298b
shinhee <tlsgml656@gmail.com>
2022-10-31 15:33:43 +0900

    파일정리 및 키보드 이벤트

--- Event/src/components/Event.jsx
+++ Event/src/components/Event.jsx
@@ -1,4 +1,4 @@
-//input 각각  onChange 함수를 따로 만듦
+//키보드 이벤트
 //rafce
 import React, {useState} from 'react'
 
@@ -17,11 +17,18 @@ const Event = () => {
     setUsername ('');
     setMessage ('');
   }
+  const onEnterPress = (e) => {
+    //엔터키 눌렀을때 클릭 함수 발생
+    if(e.key == 'Enter') {
+      onClick();
+    }
+  }
+
   return (
     <div>
       <h1>이벤트 연습</h1>
       <input type='text' name='username' placeholder='유저명' value={username} onChange={onChangeUsername}/>
-      <input type='text' name='message' placeholder='메세지' value={message}  onChange={onChangeMessage}/>
+      <input type='text' name='message' placeholder='메세지' value={message}  onChange={onChangeMessage} onKeyPress={onEnterPress}/>
       <button onClick={onClick}>확인</button>
     </div>
   )
